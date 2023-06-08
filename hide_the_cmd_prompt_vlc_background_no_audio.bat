6ff1284f4d9d16ad022a590ffe745e775c972d21
Prasad Naik <90148509+prasadnaik-null@users.noreply.github.com>
2023-06-08 10:09:00 +0530

    Update hide_the_cmd_prompt_vlc_background_no_audio.bat

--- hide_the_cmd_prompt_vlc_background_no_audio.bat
+++ hide_the_cmd_prompt_vlc_background_no_audio.bat
@@ -1,2 +1,2 @@
 @echo off
-wscript.exe "C:\null\hide_the_cmd_prompt.vbs" "C:\null\vlc_background_no_audio.bat"
+wscript.exe "C:\headphones_dont_need_to_sleep\hide_the_cmd_prompt.vbs" "C:\headphones_dont_need_to_sleep\vlc_background_no_audio.bat"
