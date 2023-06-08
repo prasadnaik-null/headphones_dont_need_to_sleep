f2ff65884505b1b3ee7e8516bea784de1fadccff
Prasad Naik <90148509+prasadnaik-null@users.noreply.github.com>
2023-06-08 10:09:14 +0530

    Update vlc_background_no_audio.bat

--- vlc_background_no_audio.bat
+++ vlc_background_no_audio.bat
@@ -1,2 +1,2 @@
 @echo off
-"%PROGRAMFILES%\VideoLAN\VLC\vlc.exe" --loop -I null "C:\null\1_minute_of_silence.mp3"
+"%PROGRAMFILES%\VideoLAN\VLC\vlc.exe" --loop -I null "C:\headphones_dont_need_to_sleep\1_minute_of_silence.mp3"
