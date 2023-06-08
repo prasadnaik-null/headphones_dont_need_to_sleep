6ede1fd0190ae1967b84fdbe7c7a114e4e601511
Prasad Naik <90148509+prasadnaik-null@users.noreply.github.com>
2023-06-08 09:52:22 +0530

    initial commit

---
+++ hide_the_cmd_prompt_vlc_background_no_audio.bat
@@ -0,0 +1,2 @@
+@echo off
+wscript.exe "C:\null\hide_the_cmd_prompt.vbs" "C:\null\vlc_background_no_audio.bat"
---
+++ vlc_background_no_audio.bat
@@ -0,0 +1,2 @@
+@echo off
+"%PROGRAMFILES%\VideoLAN\VLC\vlc.exe" --loop -I null "C:\null\1_minute_of_silence.mp3"
